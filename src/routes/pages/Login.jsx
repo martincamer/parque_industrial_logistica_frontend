39da9581ba44c544a15e50665a9191926dea94ec
martincamer <martincamer011@gmail.com>
2024-04-29 09:28:45 -0300

    ad

--- src/routes/pages/Login.jsx
+++ src/routes/pages/Login.jsx
@@ -28,15 +28,28 @@ export const Login = () => {
 
   return (
     <section className="flex items-center justify-center gap-12 h-screen  bg-white flex-col relative max-md:px-5">
+      <div className="absolute top-0 left-0 p-10">
+        <img className="w-[120px]" src="logo.png" />
+      </div>
+
       <form
         onSubmit={onSubmit}
-        className="flex w-1/3 flex-col gap-4 bg-white border-[1px] border-slate-300 px-10 py-10 rounded-xl  shadow max-md:w-full"
+        className="flex w-1/3 flex-col gap-4 bg-white border  px-10 py-12 rounded-2xl  shadow-lg shadow-slate-300 max-md:w-full"
       >
+        <div className="flex justify-center">
+          <h4 className="font-semibold text-2xl text-orange-500">
+            Te damos la bienvenida 👋
+          </h4>
+        </div>
+        <div className="text-base font-medium text-slate-500 text-center">
+          Ingresa al sistema de logistica de{" "}
+          <span className="font-bold text-slate-600">Tecnohouse</span>.
+        </div>
         {
           <div>
             <div className="flex flex-col gap-1">
               {error?.map((e) => (
-                <span className="bg-red-500/10 rounded-lg px-2 py-1 text-red-600 text-sm border-[1px] border-red-500/30">
+                <span className="bg-red-100 rounded-xl px-3 text-center uppercase py-3 text-red-800 text-sm">
                   {e}
                 </span>
               ))}
@@ -59,8 +72,11 @@ export const Login = () => {
             type={"password"}
           />
         </div>
-        <div>
+        <div className="flex w-1/3">
           <Button type={"submit"} titulo={"Iniciar Sesión"} />
+        </div>
+        <div className="text-sm font-medium text-center mt-5 w-1/2 mx-auto">
+          Si, pide a tu administrador que te cree un usuario 👀.
         </div>
       </form>
     </section>
